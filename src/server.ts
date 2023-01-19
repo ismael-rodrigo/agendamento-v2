98feed835eedc992508bbede3774bab02b7b474e
Ismael Rodrigo <86257396+ismael-rodrigo@users.noreply.github.com>
2023-01-18 23:01:21 -0300

    change server app

--- src/server.ts
+++ src/server.ts
@@ -6,12 +6,16 @@ import { routes } from "./routes/_index";
 import { errorHandler } from "./errors-handler/error-middleware-handler";
 
 
+
 const app = express()
+
 
 app.use(express.json())
 app.use(routes)
 app.use(errorHandler.handle)
 
+
 app.listen(3333 , () => console.log("Server is running") )
+
 
 export {app}
