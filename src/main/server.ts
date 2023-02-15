cd4570e641a7b5a0745ee3a1eae223a2b4ce5c0c
Ismael Rodrigo <86257396+ismael-rodrigo@users.noreply.github.com>
2023-02-14 22:27:56 -0300

    added cors config

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,13 +1,16 @@
 import "express-async-errors"
-
 import express from "express";
+import cors from 'cors';
+
 import { routes } from "./routes/_index";
 import { errorHandler } from "./middlewares/error-middleware-handler";
 
 const app = express()
 
 
-
+app.use(cors({
+    allowedHeaders:'*'
+}))
 app.use(express.json())
 app.use(routes)
 app.use(errorHandler.handle)
